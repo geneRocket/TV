0b4f0292b6896f3e27a0c0013d70ac65600cf086
generocket <generocket@github.com>
2025-02-09 18:29:55 +0800

    refactor

--- app/src/main/java/com/fongmi/android/tv/Setting.java
+++ app/src/main/java/com/fongmi/android/tv/Setting.java
@@ -439,7 +439,7 @@ public class Setting {
     }
 
     public static boolean isHomeSiteLock() {
-        return Prefers.getBoolean("home_site_lock", true);
+        return Prefers.getBoolean("home_site_lock", false);
     }
 
     public static void putHomeSiteLock(boolean lock) {
