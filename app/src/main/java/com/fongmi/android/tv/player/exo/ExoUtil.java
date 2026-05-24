ecd5bd893c8c7bc66b123794fd458119493b5034
generocket <generocket@github.com>
2026-05-24 17:23:10 +0800

    refactor

--- app/src/main/java/com/fongmi/android/tv/player/exo/ExoUtil.java
+++ app/src/main/java/com/fongmi/android/tv/player/exo/ExoUtil.java
@@ -59,7 +59,7 @@ public class ExoUtil {
 
     public static TrackSelector buildTrackSelector() {
         DefaultTrackSelector trackSelector = new DefaultTrackSelector(App.get());
-        trackSelector.setParameters(trackSelector.buildUponParameters().setPreferredTextLanguage(Locale.getDefault().getISO3Language()).setForceHighestSupportedBitrate(false).setTunnelingEnabled(Setting.isTunnel()));
+        trackSelector.setParameters(trackSelector.buildUponParameters().setPreferredTextLanguage(Locale.getDefault().getISO3Language()).setForceHighestSupportedBitrate(true).setTunnelingEnabled(Setting.isTunnel()));
         return trackSelector;
     }
 
